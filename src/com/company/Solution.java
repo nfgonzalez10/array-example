e1ffe79d16b90e604b94e4ba7d8d5f29587a7ccb
nfgonzalezc <nicolas.gonzalez@avaldigitallabs.com>
2019-10-28 16:11:30 -0500

    first commit

---
+++ src/com/company/Solution.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Solution {
+
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    System.out.println(args);
+    int n = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    int[] arr = new int[n];
+
+    String[] arrItems = scanner.nextLine().split(" ");
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    for (int i = 0; i < n; i++) {
+      int arrItem = Integer.parseInt(arrItems[i]);
+      arr[i] = arrItem;
+    }
+    for (int j = arr.length - 1; j >= 0; j--) {
+      System.out.print(arr[j] +" ");
+    }
+
+    scanner.close();
+  }
+}
+
